839bbb8e4358d55883b72abca9cca838ffc84eb3
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-01-10 18:49:57 +0100

    Add fiber

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	log.Println("Starting server on :8080")
+	if err := app.Listen(":8080"); err != nil {
+		log.Fatalf("could not start server: %v", err)
+	}
+}
