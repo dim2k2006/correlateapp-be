3d0e7868aedcf57240e87ea4b2f0ee2755a6be14
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-01-27 21:09:20 +0100

    Add measurement Service

--- pkg/domain/measurement/repository.go
+++ pkg/domain/measurement/repository.go
@@ -8,9 +8,7 @@ import (
 
 type Repository interface {
 	CreateMeasurement(ctx context.Context, measurement *Measurement) (*Measurement, error)
-	GetMeasurementByID(ctx context.Context, id uuid.UUID) (*Measurement, error)
 	ListMeasurementsByUser(ctx context.Context, userID uuid.UUID) ([]*Measurement, error)
 	ListMeasurementsByParameter(ctx context.Context, parameterID uuid.UUID) ([]*Measurement, error)
-	UpdateMeasurement(ctx context.Context, measurement *Measurement) (*Measurement, error)
 	DeleteMeasurement(ctx context.Context, id uuid.UUID) error
 }
---
+++ pkg/domain/measurement/service.go
@@ -0,0 +1,23 @@
+package measurement
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Service interface {
+	CreateMeasurement(ctx context.Context, input CreateMeasurementInput) (*Measurement, error)
+	ListMeasurementsByUser(ctx context.Context, userID uuid.UUID) ([]*Measurement, error)
+	ListMeasurementsByParameter(ctx context.Context, parameterID uuid.UUID) ([]*Measurement, error)
+	DeleteMeasurement(ctx context.Context, id uuid.UUID) error
+}
+
+type CreateMeasurementInput struct {
+	Type        Type      `json:"type"`
+	UserID      uuid.UUID `json:"userId"`
+	ParameterID uuid.UUID `json:"parameterId"`
+	Timestamp   time.Time `json:"timestamp,omitempty"`
+	Notes       string    `json:"notes,omitempty"`
+}
