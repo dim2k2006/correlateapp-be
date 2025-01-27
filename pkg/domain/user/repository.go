9c00530c5d9841aa4f9770a3d429e5fbb7e86a44
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-01-27 18:35:27 +0100

    Add Repository

---
+++ pkg/domain/user/repository.go
@@ -0,0 +1,15 @@
+package user
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type Repository interface {
+	Create(ctx context.Context, user *User) (*User, error)
+	GetUserByID(ctx context.Context, id uuid.UUID) (*User, error)
+	GetUserByExternalID(ctx context.Context, externalID string) (*User, error)
+	UpdateUser(ctx context.Context, user *User) (*User, error)
+	DeleteUser(ctx context.Context, id uuid.UUID) error
+}
