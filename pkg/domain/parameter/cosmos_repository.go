1fc5d5af7aaaa8d38d7c6c7e583df6a6a8151944
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-02-12 20:22:18 +0100

    Add CreateParameter function

--- pkg/domain/parameter/cosmos_repository.go
+++ pkg/domain/parameter/cosmos_repository.go
@@ -1,9 +1,13 @@
 package parameter
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/data/azcosmos"
+	"github.com/google/uuid"
 )
 
 const (
@@ -32,4 +36,57 @@ func NewCosmosParameterRepository(connectionString string) (*CosmosParameterRepo
 		client:    client,
 		container: container,
 	}, nil
+}
+
+func (r *CosmosParameterRepository) CreateParameter(ctx context.Context, parameter *Parameter) (*Parameter, error) {
+	parameterJSON, err := json.Marshal(NewCosmosParameter(parameter))
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal parameter: %w", err)
+	}
+
+	pk := azcosmos.NewPartitionKeyString(parameter.ID.String())
+
+	_, err = r.container.CreateItem(ctx, pk, parameterJSON, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create parameter in Cosmos DB: %w", err)
+	}
+
+	return parameter, nil
+}
+
+type CosmosParameter struct {
+	ID          uuid.UUID `json:"id"`
+	UserID      uuid.UUID `json:"userId"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	DataType    DataType  `json:"dataType"`
+	Unit        string    `json:"unit"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+}
+
+func NewCosmosParameter(parameter *Parameter) *CosmosParameter {
+	return &CosmosParameter{
+		ID:          parameter.ID,
+		UserID:      parameter.UserID,
+		Name:        parameter.Name,
+		Description: parameter.Description,
+		DataType:    parameter.DataType,
+		Unit:        parameter.Unit,
+		CreatedAt:   parameter.CreatedAt,
+		UpdatedAt:   parameter.UpdatedAt,
+	}
+}
+
+func NewParameter(cosmosParameter *CosmosParameter) *Parameter {
+	return &Parameter{
+		ID:          cosmosParameter.ID,
+		UserID:      cosmosParameter.UserID,
+		Name:        cosmosParameter.Name,
+		Description: cosmosParameter.Description,
+		DataType:    cosmosParameter.DataType,
+		Unit:        cosmosParameter.Unit,
+		CreatedAt:   cosmosParameter.CreatedAt,
+		UpdatedAt:   cosmosParameter.UpdatedAt,
+	}
 }
