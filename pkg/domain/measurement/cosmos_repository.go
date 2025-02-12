1b56a84f11a61834ad39e791837bac1a842082c6
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-02-12 21:16:30 +0100

    Add CreateMeasurement method

--- pkg/domain/measurement/cosmos_repository.go
+++ pkg/domain/measurement/cosmos_repository.go
@@ -1,9 +1,13 @@
 package measurement
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/data/azcosmos"
+	"github.com/google/uuid"
 )
 
 const (
@@ -32,4 +36,75 @@ func NewCosmosMeasurementRepository(connectionString string) (*CosmosMeasurement
 		client:    client,
 		container: container,
 	}, nil
+}
+
+func (r *CosmosMeasurementRepository) CreateMeasurement(
+	ctx context.Context,
+	measurement Measurement,
+) (Measurement, error) {
+	measurementJSON, err := json.Marshal(NewCosmosMeasurement(measurement))
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal measurement: %w", err)
+	}
+
+	pk := azcosmos.NewPartitionKeyString(measurement.GetParameterID().String())
+
+	_, err = r.container.CreateItem(ctx, pk, measurementJSON, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create measurement in Cosmos DB: %w", err)
+	}
+
+	return measurement, nil
+}
+
+type CosmosMeasurement struct {
+	Type        DataType    `json:"type"`
+	ID          uuid.UUID   `json:"id"`
+	UserID      uuid.UUID   `json:"userId"`
+	ParameterID uuid.UUID   `json:"parameterId"`
+	Timestamp   time.Time   `json:"timestamp"`
+	Notes       string      `json:"notes"`
+	CreatedAt   time.Time   `json:"createdAt"`
+	UpdatedAt   time.Time   `json:"updatedAt"`
+	Value       interface{} `json:"value"`
+}
+
+func NewCosmosMeasurement(m Measurement) *CosmosMeasurement {
+	switch m.GetType() {
+	case DataTypeFloat:
+		return &CosmosMeasurement{
+			Type:        m.GetType(),
+			ID:          m.GetID(),
+			UserID:      m.GetUserID(),
+			ParameterID: m.GetParameterID(),
+			Timestamp:   m.GetTimestamp(),
+			Notes:       m.GetNotes(),
+			CreatedAt:   m.GetCreatedAt(),
+			UpdatedAt:   m.GetUpdatedAt(),
+			Value:       m.(*FloatMeasurement).Value,
+		}
+	default:
+		return nil
+	}
+}
+
+func NewMeasurement(m *CosmosMeasurement) Measurement {
+	switch m.Type {
+	case DataTypeFloat:
+		return &FloatMeasurement{
+			BaseMeasurement: BaseMeasurement{
+				Type:        m.Type,
+				ID:          m.ID,
+				UserID:      m.UserID,
+				ParameterID: m.ParameterID,
+				Timestamp:   m.Timestamp,
+				Notes:       m.Notes,
+				CreatedAt:   m.CreatedAt,
+				UpdatedAt:   m.UpdatedAt,
+			},
+			Value: m.Value.(float64),
+		}
+	default:
+		return nil
+	}
 }
